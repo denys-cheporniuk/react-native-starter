46b97f590ec37cdfefbc50d440f09745a6459088
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-09-30 12:29:40 +0300

    add navigation to the application

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import {Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen 123</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to components"
+      />
+
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to the list"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
